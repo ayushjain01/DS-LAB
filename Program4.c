122cd9a7000f2ebe95b369b3cf5b741391236aac
Ayush Jain <ayushganna67@gmail.com>
2021-10-18 19:40:23 +0530

    Created Program4.c

---
+++ Program4.c
@@ -0,0 +1,33 @@
+//Array of structs using malloc
+#include<stdio.h>
+#include <stdlib.h>
+struct student
+    {
+        char fname[50];
+        int roll;
+
+    } ;
+
+int main()
+{
+    
+    int n;
+    printf("Enter number of students-");
+    scanf("%d",&n);
+    int i;
+    struct student* students = malloc(n * sizeof(struct student));
+    for (i = 0; i < n; i++)
+    {
+
+        printf("Enter first name :");
+        scanf("%s",students[i].fname);
+        printf("Enter roll number  :");
+        scanf("%d",&students[i].roll);
+
+    }
+
+    for (i = 0; i < n; i++)
+        printf("First Name: %s, Roll number: %d\n",students[i].fname,students[i].roll);
+
+    return (0);
+}
