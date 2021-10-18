aa209589a7354173d7dad920cb9fe973972ed62c
Ayush Jain <ayushganna67@gmail.com>
2021-10-18 20:47:48 +0530

    Update Program1.c

--- Program1.c
+++ Program1.c
@@ -1,21 +1,31 @@
 /*Pass by Value, Pass by Reference, Global and Local Variable*/
 
 #include <stdio.h>
-int a = 10;   //gloabl variable
-int fun1(int num)
-{
-    return num + a;
-}
-int fun2(int *num)
-{
-    int a = 20;//local variable
-    return *num + a;
-}
-int main() {
-    int num = 100;  //local variable
-    int res1,res2;
-    res1 = fun1(num); //pass by value
-    res2 = fun2(&num); //pass by reference
-    printf("Result 1 = %d\nResult 2 = %d",res1,res2);
-    return 0;
-}
+int n = 10;  //global variable
+void fun1 (int a, int b)  
+{  //local variables 
+    int temp;   
+    temp = a;  
+    a=b;  
+    b=temp;  
+    printf("Values in fun1 a = %d, b = %d\n",a,b); 
+}  
+void fun2(int *a) {    
+    printf("Before adding value inside fun2 num=%d \n",*a); 
+    (*a) += n;    
+    printf("After adding value inside func2 num=%d \n", *a);    
+}      
+
+int main()  
+{  
+    int a = 10;  
+    int b = 20;  
+    int x=100;  
+    printf("Before calling fun1 a = %d, b = %d\n",a,b); 
+    fun1(a,b);  
+    printf("After calling fun1 a = %d, b = %d\n",a,b); 
+      
+    printf("Before calling fun2 x=%d \n", x);    
+    fun2(&x);  
+    printf("After calling fun2 x=%d \n", x); 
+}  
